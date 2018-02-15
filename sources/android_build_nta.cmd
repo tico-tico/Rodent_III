4aea2d07c52c4efc29eb4b18285d4c018188ae65
tico-tico <sergei.ivn@gmx.com>
2018-02-15 23:43:14 +0300

    switching from `clang++` to `clang`

--- sources/android_build_nta.cmd
+++ sources/android_build_nta.cmd
@@ -26,10 +26,14 @@ goto :eof
 
 :build
 
+echo Building for Android...
+
 set PATH=%ANDROID_TOOLS_DIR%\bin;%PATH%
 
 copy /b src\*.cpp src\combined_a.cpp > nul
 
-call clang++ -Ofast -s -std=c++14 -march=armv7-a -mfpu=neon -fPIE -fPIC -fno-rtti -fno-stack-protector -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-ident -DNO_THREADS -DUSEGEN -DNDEBUG -DBOOKSPATH=/sdcard/RodentIII/books/ -DPERSONALITIESPATH=/sdcard/RodentIII/personalities/ src\combined_a.cpp -static-libstdc++ -Wl,--fix-cortex-a8,-pie -o rodentiii_arm7a_neon_nta
+rem call clang++ -Ofast -s -std=c++14 -march=armv7-a -mfpu=neon -fPIE -fPIC -fno-rtti -fno-stack-protector -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-ident -DUSE_THREADS -DUSEGEN -DNDEBUG -D_FORTIFY_SOURCE=0 -DBOOKSPATH=/sdcard/RodentIII/books/ -DPERSONALITIESPATH=/sdcard/RodentIII/personalities/ src\combined_a.cpp -static-libstdc++ -Wl,--fix-cortex-a8,-pie -o rodentiii_arm7a_neon_nta
+
+call clang -Ofast -s -std=c++14 -march=armv7-a -mfpu=neon -fPIE -fPIC -fno-rtti -fno-stack-protector -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-ident -lm -DNO_THREADS -DUSEGEN -DNDEBUG -D_FORTIFY_SOURCE=0 -DBOOKSPATH=/sdcard/RodentIII/books/ -DPERSONALITIESPATH=/sdcard/RodentIII/personalities/ src\combined_a.cpp -Wl,--fix-cortex-a8,-pie -o rodentiii_arm7a_neon_nta
 
 del /q src\combined_a.cpp
